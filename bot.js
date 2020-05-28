5057c60770d2681c013049c72df85617832cb48c
Jinx-and-Taco <66083115+Jinx-and-Taco@users.noreply.github.com>
2020-05-28 11:38:45 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,213 @@ client.on('message', message => {
   	}
 });
 
+// THIS IS THE @Liquid Chris COMMAND
+
+client.on('message', message => {
+    if (message.content === '!Liquid Chris') {
+    	message.reply('She is the best mod!');
+  	}
+});
+
+// THIS IS THE secret!raid COMMAND
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'secret!raid') {
+    	message.reply('hi');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
